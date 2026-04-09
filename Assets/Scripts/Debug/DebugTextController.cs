a2afda496e55d3ef92abdab4e9c64e4963b5c152
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-04-09 15:24:12 -0500

    basic debug text ui

---
+++ Assets/Scripts/Debug/DebugTextController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class DebugTextController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _debugUIText;
+
+    private PlayerBehaviors pb;
+    private Rigidbody2D rb;
+    private PlayerController pc;
+
+    void Start()
+    {
+        pb = FindAnyObjectByType<PlayerBehaviors>();
+        pc = FindAnyObjectByType<PlayerController>();
+        rb = pb.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDebugText();
+    }
+
+    public void UpdateDebugText()
+    {
+        string output = "velocity: " + rb.linearVelocity;
+
+        _debugUIText.text = output;
+    }
+}
