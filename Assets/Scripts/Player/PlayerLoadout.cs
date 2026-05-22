b96124a92c2d814662e1d12407e708c573bc64b4
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-05-22 18:09:40 -0400

    ability to double jump checked in PlayerLoadout

--- Assets/Scripts/Player/PlayerLoadout.cs
+++ Assets/Scripts/Player/PlayerLoadout.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerLoadout : MonoBehaviour
 {
-    [SerializeField] private bool doubleJumpUnlocked = true;
+    [SerializeField] private bool doubleJumpUnlocked = false;
 
     public bool DoubleJumpUnlocked { get => doubleJumpUnlocked; set => doubleJumpUnlocked = value; }
 }
