194d8472973d420e9464e8c6c1c5fae471a88bf8
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-05-28 13:02:17 -0400

    filled out InteractibleEntity.cs

--- Assets/Scripts/AbstractClasses/InteractableEntity.cs
+++ Assets/Scripts/AbstractClasses/InteractableEntity.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
-public class InteractableEntity : MonoBehaviour
+public abstract class InteractableEntity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        TrySubscribeInteraction(collision);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerExit2D(Collider2D collision)
     {
-        
+        TryUnsubscribeInteraction(collision);
+    }
+
+    private void OnDestroy()
+    {
+        FindAnyObjectByType<PlayerBehaviors>().interactAction -= OnInteract;
+    }
+
+    public virtual void TrySubscribeInteraction(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out PlayerBehaviors pb))
+        {
+            pb.interactAction += OnInteract;
+        }
+    }
+
+    public virtual void TryUnsubscribeInteraction(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out PlayerBehaviors pb))
+        {
+            pb.interactAction -= OnInteract;
+        }
+    }
+
+    public virtual void OnInteract()
+    {
+
     }
 }
