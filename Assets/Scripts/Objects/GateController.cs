5c11c3f5bad7392f8f5cdb19460281de78312b16
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-06-03 12:09:52 -0400

    player now parents to platform

--- Assets/Scripts/Objects/GateController.cs
+++ Assets/Scripts/Objects/GateController.cs
@@ -50,19 +50,5 @@ public class GateController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.TryGetComponent<PlayerBehaviors>(out PlayerBehaviors pb))
-        {
-            collision.transform.SetParent(transform);
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.TryGetComponent<PlayerBehaviors>(out PlayerBehaviors pb))
-        {
-            collision.transform.SetParent(null);
-        }
-    }
+    
 }
