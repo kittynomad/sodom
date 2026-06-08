df314cc3fd96bab6de2d2c36b9dad6f6a34576cb
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-06-07 20:26:19 -0400

    added logic for respawning player in correct position

--- Assets/Scripts/Singletons/SpawnPointHolder.cs
+++ Assets/Scripts/Singletons/SpawnPointHolder.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SpawnPointHolder : MonoBehaviour
 {
     public static SpawnPointHolder instance;
+
+    private Dictionary<string, Vector2> sceneSpawnPoints;
+
+    public Dictionary<string, Vector2> SceneSpawnPoints { get => sceneSpawnPoints; set => sceneSpawnPoints = value; }
+
     public void Awake()
     {
         if(instance != null && instance != this)
@@ -15,6 +21,30 @@ public class SpawnPointHolder : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void UpdateSpawnPoint(Vector2 pos)
+    {
+        if(sceneSpawnPoints.ContainsKey(SceneManager.GetActiveScene().name))
+        {
+            sceneSpawnPoints.Remove(SceneManager.GetActiveScene().name);
+        }
+
+        sceneSpawnPoints.Add(SceneManager.GetActiveScene().name, pos);
+    }
+
+    public bool GetRelevantSpawnPoint(out Vector2 pos)
+    {
+        if(sceneSpawnPoints.ContainsKey(SceneManager.GetActiveScene().name))
+        {
+            pos = sceneSpawnPoints[SceneManager.GetActiveScene().name];
+            return true;
+        }
+        else
+        {
+            pos = Vector2.zero;
+            return false;
         }
     }
 }
