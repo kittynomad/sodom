1aaa8263833860453e8f943df32bfd00ac1e480d
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-07-04 15:39:07 -0500

    money dropper set up and stuff

--- Assets/Scripts/Objects/MoneyDroppingObject.cs
+++ Assets/Scripts/Objects/MoneyDroppingObject.cs
@@ -15,13 +15,31 @@ public class MoneyDroppingObject : MonoBehaviour
     [MinMaxSlider(0f, 1000f)] [SerializeField]
     private Vector2 _moneyRange;
 
+    [MinMaxSlider(0f, 250f)]
+    [SerializeField] private Vector2 _coinLaunchStrength;
+
     [SerializeField] private float[] _targetDivisions;
 
     [SerializeField] private GameObject _moneyPrefab;
 
+    [Button]
     public void DropMoney()
     {
         float moneyToDrop = Random.Range(_moneyRange.x, _moneyRange.y);
         System.Array.Sort(_targetDivisions);
+        for(int i = _targetDivisions.Length - 1; i >= 0; i--)
+        {
+            if(moneyToDrop / _targetDivisions[i] > 1f)
+            {
+                for(int j = 0; j < (int)(moneyToDrop / _targetDivisions[i]); j++)
+                {
+                    GameObject temp = Instantiate(_moneyPrefab, transform.position, Quaternion.identity);
+                    temp.GetComponent<Rigidbody2D>().linearVelocity = Random.Range(_coinLaunchStrength.x, _coinLaunchStrength.y)
+                        * Vector2.Normalize(new Vector2(Random.Range(-1f, 1f), Random.Range(0, 1f)));
+
+                    temp.GetComponent<CurrencyObject>().UpdateValue(_targetDivisions[i]);
+                }
+            }
+        }
     }
 }
