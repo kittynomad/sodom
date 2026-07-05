3e8586499baf6b40481bbc8d933f014c6bd1ae5d
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-07-05 11:51:06 -0500

    re-imported CharacterData from another project

--- Assets/Scripts/ScriptableObjects/CharacterData.cs
+++ Assets/Scripts/ScriptableObjects/CharacterData.cs
@@ -12,21 +12,47 @@ using UnityEngine;
 [CreateAssetMenu]
 public class CharacterData : ScriptableObject
 {
+    //name is how character is labeled in-game, doesn't need to be unique
     [SerializeField] private string _characterName;
-    [TextArea(3, 10)] [SerializeField] private string _characterBio;
-    [SerializeField] private Sprite _characterIcon;
+
+    //characterID is used internally, must be unique and should be camelCase
+    [SerializeField] private string _characterID;
+
+    [Header("Character Assets")]
+    //portrait to use if no portrait ID is specified
     [SerializeField] private Sprite _defaultCharacterPortrait;
     [SerializeField] private Portrait[] _portraits;
+
+    //list of sound bytes to play while character is speaking
     [SerializeField] private RandomizedAudio _characterVoice;
-    [SerializeField] private Color _textColor = Color.black;
+
+    [TextArea(3, 10)] [SerializeField] private string _characterBio;
+
+    [Space(10)]
+
+    [Header("Character Stats")]
+    [SerializeField] private int _characterHealth;
+    [SerializeField] private int _attackPower;
+
+    [Space(10)]
+
+    [Header("Character Tags")]
+    //Interaction types are the ways in which a character can be added to the bestiary
+    //[SerializeField] private Enums.InteractType[] _interactionTypes;
+    //Character types are primarily for organization
+    //[SerializeField] private Enums.CharacterType[] _characterTypes;
+    [SerializeField] private bool _appearsInBestiary = true;
 
     public string CharacterName { get => _characterName; set => _characterName = value; }
     public Sprite DefaultCharacterPortrait { get => _defaultCharacterPortrait; set => _defaultCharacterPortrait = value; }
     public RandomizedAudio CharacterVoice { get => _characterVoice; set => _characterVoice = value; }
     public Portrait[] Portraits { get => _portraits; set => _portraits = value; }
     public string CharacterBio { get => _characterBio; set => _characterBio = value; }
-    public Sprite CharacterIcon { get => _characterIcon; set => _characterIcon = value; }
-    public Color TextColor { get => _textColor; set => _textColor = value; }
+    public int CharacterHealth { get => _characterHealth; set => _characterHealth = value; }
+    public int AttackPower { get => _attackPower; set => _attackPower = value; }
+    //public Enums.InteractType[] InteractionTypes { get => _interactionTypes; set => _interactionTypes = value; }
+    //public Enums.CharacterType[] CharacterTypes { get => _characterTypes; set => _characterTypes = value; }
+    public bool AppearsInBestiary { get => _appearsInBestiary; set => _appearsInBestiary = value; }
 
     /// <summary>
     /// gets a portrait of the character from its associated ID. if no match is
@@ -36,9 +62,9 @@ public class CharacterData : ScriptableObject
     /// <returns>the portrait associated with the ID.</returns>
     public Sprite GetPortraitByID(string id)
     {
-        foreach(Portrait p in _portraits)
+        foreach (Portrait p in _portraits)
         {
-            if(p.PortraitID.Equals(id))
+            if (p.PortraitID.Equals(id))
             {
                 return p.PortraitImage;
             }
@@ -46,4 +72,12 @@ public class CharacterData : ScriptableObject
         Debug.Log("no portrait matching ID found, returning default");
         return _defaultCharacterPortrait;
     }
+
+    /*public string FormattedTagString()
+    {
+        string output = "Tags: ";
+        foreach (Enums.CharacterType t in _characterTypes)
+            output = output + t + "; ";
+        return output;
+    }*/
 }
