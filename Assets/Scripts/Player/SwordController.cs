21bc1d50ec747d4d02f4f101ff859d34cc6e1531
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-08-13 17:41:46 -0500

    goo

--- Assets/Scripts/Player/SwordController.cs
+++ Assets/Scripts/Player/SwordController.cs
@@ -14,6 +14,7 @@ public class SwordController : MonoBehaviour
     private GameObject attachedObject;
     //speed which corpses are jettisoned when detached
     [SerializeField] private float detachSpeed = 5f;
+    [SerializeField] private float _hitDamage = 1f;
     [SerializeField] private GameObject _meleeChewFX;
 
     public bool HasCorpseAttached { get => attachedObject != null; }
@@ -38,7 +39,7 @@ public class SwordController : MonoBehaviour
             Vector2 contactPoint = collision.ClosestPoint(transform.position);
             Quaternion customRotation = Quaternion.Euler(0f, 0f, Random.Range(80f, 100f));
             Instantiate(_meleeChewFX, contactPoint, customRotation);
-            ik.OnDamage(1f, gameObject);
+            ik.OnDamage(_hitDamage, gameObject);
         }
     }
 
