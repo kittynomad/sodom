c23a26718b3ae7d879984b0faebfc22ab959abda
PierceNunn <agentp5151@gmail.com>
2026-09-25 14:06:43 -0500

    made damage dealt by ProjectileController an exposed variable

--- Assets/Scripts/Objects/ProjectileController.cs
+++ Assets/Scripts/Objects/ProjectileController.cs
@@ -10,11 +10,13 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
+    [SerializeField] private float _projectileDamage = 1f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent(out IKillable ik))
         {
-            ik.OnDamage(1f, gameObject);
+            ik.OnDamage(_projectileDamage, gameObject);
         }
         Destroy(gameObject);
     }
