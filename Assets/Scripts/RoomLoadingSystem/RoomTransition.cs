001b5970dfcafe427c6bcef5ed74be33e213ebb9
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-09-25 15:28:23 -0500

    Scene Setup

---
+++ Assets/Scripts/RoomLoadingSystem/RoomTransition.cs
@@ -0,0 +1,18 @@
+/************************************************************************
+// File Name :RoomTransition.cs
+// Author : Arcadia Koederitz
+// Creation Date : 9/25/2026
+// Last Modified : 9/25/2026
+//
+// Brief Description : Triggers transitions between rooms and prompts the LevelStreamingService to load the 
+// appropriate levels.
+*****************************************************************************/
+using NaughtyAttributes;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour
+{
+    [SerializeField, Scene] private int _destinationScene;
+    [SerializeField, Range(0, 10)] private byte _doorIndex;
+    [SerializeField, Range(0, 10)] private byte _destinationIndex;
+}
