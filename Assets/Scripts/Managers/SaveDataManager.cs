df2c0b1dde95e6f2008511a0876ac79a746ea5a8
PierceNunn <agentp5151@gmail.com>
2026-09-25 15:49:39 -0500

    created SaveDataManager

---
+++ Assets/Scripts/Managers/SaveDataManager.cs
@@ -0,0 +1,14 @@
+/*****************************************************************************
+// File Name : SaveDataManager.cs
+// Author : Pierce Nunnelley
+// Creation Date : February 13, 2026
+//
+// Brief Description : This script initializes services for saved data.
+*****************************************************************************/
+using UnityEngine;
+using TFOOL.ManagersAndServices;
+
+public class SaveDataManager : Manager
+{
+    
+}
